05cf48cc86f5773fc0e92c4daf397d4e68df4115
Antoine Gogce <antoinegogceapb@hotmail.fr>
2023-01-19 14:03:30 +0100

    init + header + footer

---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,24 @@
+<template>
+    <footer>Copyright &copy; {{ annee }}  - Moi
+    </footer>
+    
+</template>
+  
+<script>
+  export default({
+    name: 'MyFooter',
+    data() {
+      return {
+        //créer une variable JS pour afficher l'année en dynamique
+        annee: new Date().getFullYear()
+      }
+    },
+  
+  })
+</script>
+  
+<style scoped>
+  footer {
+    text-align: center;
+  }
+</style>
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,14 @@
+<template>
+<h1>Vue Convertisseur</h1>
+<img src="../assets/images/vuejs_original_logo_icon_146304.png" alt="icone vuejs" class="logo">
+</template>
+
+<script>
+
+</script>
+
+
+
+<style scoped>
+
+</style>
